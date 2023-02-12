75d0256e8362de6a9402c7e412f1057c269a209e
priyankajadhav25 <jadhavpriyankah25@gmail.com>
2023-02-12 08:54:29 +0530

    JS_Task1 upload

---
+++ JS_Task1/VariableType.js
@@ -0,0 +1,15 @@
+//Program to find the datatype of variable
+
+var name="Priyanka";
+var age=19;
+var marks=85.99;
+var student=true;
+var x;
+var y=null;
+
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(marks));
+console.log(typeof(student));
+console.log(typeof(x));
+console.log(typeof(null));
